f0c91646709e9dccb58763a7c4ab5f11b236eab1
Daniel Palacios <daniel80380@gmail.com>
2023-02-01 16:39:37 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,20 +1,36 @@
+import { useSelector } from "react-redux";
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled3 = () => {
+const Untitled3 = ({
+  navigation
+}) => {
+  const UserDetails = useSelector(state => state.UserDetails);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable onPress={() => navigation.navigation.navigate("Untitled4", {
+        "name": UserDetails.username,
+        "surname": "palacios"
+      })}><View style={styles.PdFFgUFL}></View></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  PdFFgUFL: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled3;
